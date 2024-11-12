a0f496d74727750424e69b209f2a16616a22d68a
go2023 <tgoncharenko2013@gmail.com>
2024-11-12 13:34:22 -0800

    navbar-app elements were implemented

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,13 +6,17 @@ const http = require('http');
 const { readFileSync } = require('fs');
 
 // get all files
-const homePage = readFileSync('./index.html');
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
     // console.log('user hits a server')
     // console.log(req.method)
     // console.log(req.url)
     const url = req.url
+    console.log(url)
     // home page
     if(url === '/') {
         res.writeHead(200, {'content-type': 'text/html'})
@@ -24,6 +28,24 @@ const server = http.createServer((req, res) => {
     else if(url === '/about') {
         res.writeHead(200, {'content-type': 'text/html'})
         res.write('<h1>Welcome to our about page</h1>')
+        res.end()
+    }
+    // styles
+    else if (url === '/styles.css') {
+        res.writeHead(200, { 'content-type': 'text/css' })
+        res.write(homeStyles)
+        res.end()
+    }
+    // image/logo
+    else if (url === '/logo.svg') {
+        res.writeHead(200, { 'content-type': 'image/svg+xml' })
+        res.write(homeImage)
+        res.end()
+    }
+    // logic
+    else if (url === '/browser-app.js') {
+        res.writeHead(200, { 'content-type': 'text/javascriptl' })
+        res.write(homeLogic)
         res.end()
     }
     // 404 page
