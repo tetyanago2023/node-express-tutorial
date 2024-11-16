4077b9a4b0c32d00280e9801ce2a5179231ba30a
go2023 <tgoncharenko2013@gmail.com>
2024-11-15 18:25:23 -0800

    files in method-public folder were created

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,8 +4,15 @@ console.log('Express Tutorial');
 
 const express = require('express');
 const app = express();
+let { people } = require('./data');
+
+// static assets
+app.use(express.static('./methods-public'));
 
 
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data: people});
+})
 app.listen(5000, () => {
     console.log('Server is running on http://localhost:5000');
 })
