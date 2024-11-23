cd99c114a27d4492d5b49ebf13cbc8508c7b7a74
go2023 <tgoncharenko2013@gmail.com>
2024-11-22 21:41:34 -0800

    controller and routes were created

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,2 +1,29 @@
 // tasks.js
 
+const getAllTasks = (req, res) => {
+    res.send('get all tasks');
+}
+
+const createTask = (req, res) => {
+    res.send('create task');
+}
+
+const getTask = (req, res) => {
+    res.send('get single task');
+}
+
+const updateTask = (req, res) => {
+    res.send('update task');
+}
+
+const deleteTask = (req, res) => {
+    res.send('delete task');
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
