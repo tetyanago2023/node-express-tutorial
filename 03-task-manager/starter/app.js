63587ab85bd7fe14cb7fb3ae80ac3c9bb89d8fb6
go2023 <tgoncharenko2013@gmail.com>
2024-11-22 18:18:21 -0800

    moving to tasks

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,3 +1,22 @@
 // app.js
 
 console.log('Task Manager App')
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const tasks = require('./routes/tasks');
+
+app.use(express.static('./public'));
+app.use(express.json());
+app.use('/api/v1/tasks', tasks);
+
+app.get('/hello', (req, res) => {
+    res.status(200).send('Task Manager App');
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ 03-task-manager/starter/routes/tasks.js
@@ -0,0 +1,11 @@
+// tasks.js
+
+const express = require('express');
+const router = express.Router();
+
+
+router.route('/').get((req, res) => {
+    res.send('all items');
+})
+
+module.exports = router;
