00deda4c01ae35549373f854964cbf3fc6b002dd
go2023 <tgoncharenko2013@gmail.com>
2024-11-30 22:39:58 -0800

    app.listen was implemented

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,3 +1,13 @@
 // app.js
 
 console.log('04 Store API')
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
