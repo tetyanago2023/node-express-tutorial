bd2fb07586c538e212af345ce27343c662eb490c
go2023 <tgoncharenko2013@gmail.com>
2024-12-01 16:43:41 -0800

    controllers and productsRouter were created

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -4,10 +4,40 @@ console.log('04 Store API')
 
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+// async errors
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href = "/api/v1/products">products route</a>');
+})
+app.use('/api/v1/products', productsRouter);
+
+// products route
 
 
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const start = async () => {
+    try {
+        // connectDB
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,2 +1,15 @@
-// products.js
+// controllers/products.js
 
+
+const getAllProductsStatic = async (req, res) => {
+    res.status(200).json({msg: 'products testing route'});
+}
+
+const getAllProducts = async (req, res) => {
+    res.status(200).json({msg: 'products route'});
+}
+
+module.exports = {
+    getAllProductsStatic,
+    getAllProducts
+}
