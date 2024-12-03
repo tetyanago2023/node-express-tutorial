903baf29f3069f7f8ec51eb5ec07e54858cf6df6
go2023 <tgoncharenko2013@gmail.com>
2024-12-02 18:57:01 -0800

    search functionality was adjusted

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -4,19 +4,28 @@ const Product = require('../models/product');
 
 
 const getAllProductsStatic = async (req, res) => {
-    // const products = await Product.find({ name: 'wooden table' });
-    const products = await Product.find({ page: '2' });
+    const products = await Product.find({ name: 'wooden table' });
+    // const products = await Product.find({ page: '2' });
     res.status(200).json({ products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured } = req.query;
+    const { featured, company, name } = req.query;
 
     const queryObject = {};
 
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
     }
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (name) {
+        queryObject.name = name;
+    }
+
     console.log(queryObject);
     const products = await Product.find(queryObject);
     res.status(200).json({ products, nbHits: products.length });
