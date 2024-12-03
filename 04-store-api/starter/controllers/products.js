2886bc22d0147e2545d51b6a23218bac2a124298
go2023 <tgoncharenko2013@gmail.com>
2024-12-03 15:53:18 -0800

    skipping functionality was implemented

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -13,8 +13,19 @@ const getAllProductsStatic = async (req, res) => {
     // const products = await Product.find({}).sort('name'); // sort by name
     // const products = await Product.find({}).sort('-name'); // sort by name
     // const products = await Product.find({}).sort('-name price'); // sort by name & price
-    const products = await Product.find({}).select('name price'); // select some fields
-    res.status(200).json({ products, nbHits: products.length });
+    // const products = await Product.find({}).select('name price'); // select some fields
+    // const products = await Product.find({}).select('name price').limit(4); // select some fields & show only 4 first items
+    // const products = await Product.find({})
+    //     .sort('name')
+    //     .select('name price')
+    //     .limit(10); // sort by name select some fields & show only 10 first items
+    const products = await Product.find({})
+        .sort('name')
+        .select('name price')
+        .limit(10)
+        .skip(1); // sort by name select some fields & show only 10 first items & skip the first item
+    // res.status(200).json({ products, nbHits: products.length });
+    res.status(200).json({ nbHits: products.length, products });
 }
 
 const getAllProducts = async (req, res) => {
