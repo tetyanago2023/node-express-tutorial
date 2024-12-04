5d66cc6e4f4f4119421a677cb3bf86010b8b8d7d
go2023 <tgoncharenko2013@gmail.com>
2024-12-03 16:55:45 -0800

    numericFilters functionality was adjusted

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -58,12 +58,19 @@ const getAllProducts = async (req, res) => {
             '=': '$eq',
             '<': '$lt',
             '<=': '$lte',
-        }
-        const regEx = /\b(>|>=|=|<|<=)\b/g;
-        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
-
-        // console.log(numericFilters);
-        console.log(filters);
+        };
+        const regEx = /\b(<|>|>=|=|<=)\b/g;
+        let filters = numericFilters.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+        );
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-');
+            if (options.includes(field)) {
+                queryObject[field] = { [operator]: Number(value) };
+            }
+        });
     }
     console.log(queryObject);
     // const products = await Product.find(queryObject); // find products by query w/o sorting
