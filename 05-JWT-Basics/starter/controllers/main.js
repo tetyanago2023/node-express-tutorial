91315d239f81a87a81e7316fe1dad2515d711e8b
go2023 <tgoncharenko2013@gmail.com>
2024-12-05 21:41:25 -0800

    authentication functionality was created

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,21 +1,42 @@
 // controllers/main.js
 
-const CustomAPIError = require('../Errors/custom-error');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
-    const { name, password } = req.body;
+    const { username, password } = req.body;
 
-    if (!name || !password) {
+    if (!username || !password) {
         throw new CustomAPIError(`Please provide name and password`, 400)
     }
+// just a demo, usually provided by the db
+    const id = new Date().getDate();
 
-    res.send(`Fake Login/Register/Signup Route`)
+// try to keep payload small, better experience for user
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
+
+    res.status(200).json({msg: `User created`, token});
 }
 
 const dashboard = async (req, res) => {
-    const luckyNumber = Math.floor(Math.random() * 100)
-    res.status(200).json({msg: `Hello, John doe!`,
-        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
+    // console.log(req.headers);
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomAPIError(`No token provided`, 401)
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const luckyNumber = Math.floor(Math.random() * 100)
+        res.status(200).json({msg: `Hello, ${decoded.username}!`,
+            secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
+    } catch (error){
+        throw new CustomAPIError(`Not authorized to access this route`, 401)
+    }
 }
 
 module.exports = {
--- 05-JWT-Basics/starter/middleware/auth.js
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -1,2 +1,11 @@
 // auth.js
 
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
+
+const authenticationMiddleware = async (req, res, next) => {
+    console.log(req.headers.authorization);
+    next();
+}
+
+module.exports = authenticationMiddleware;
